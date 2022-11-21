edcff9bfd02b30af0d90a2abd76575402fb5387c
DoIT Technology <maurycy0621@gmail.com>
2022-11-21 08:06:11 +0100

    Dokonane zmiany 21.11.2022

--- db/playlist.js
+++ db/playlist.js
@@ -17,18 +17,31 @@ async function getAllSongs(playlista) {
     return await knex('songs').select("*").where("playlist_id", playlist_id);
 };
 
-async function insertSong(name) {
-    const rekord = await knex("playlist").select("id").where("name", name).first();
+async function insertSong(playlista, putData) {
+    const playlist_space = await knex("playlist").select("*").where("name", playlista).first();
+    const playlist_id = playlist_space.id;
 
-    if (!rekord)
+    if (playlist_id)
     {
-        return await knex('playlist').insert({name});
+        return await knex('songs').insert({song_name: putData.song_name,src_link: putData.src_link,playlist_id: playlist_id}).then(()=>{});
     };
-    throw new Error("Podana playlista już istnieje w bazie danych!");
+};
+
+async function getAllPlaylists() {
+    return await knex('playlist').select('id','name');
+};
+
+async function deletePlaylist(playlista) {
+    const playlist_space = await knex("playlist").select("*").where("name", playlista).first();
+    const playlist_id = playlist_space.id;
+    console.log(playlist_space);
+    console.log(playlist_id);
 };
 
 module.exports = {
     createPlaylist,
     getAllSongs,
-    insertSong
+    insertSong,
+    getAllPlaylists,
+    deletePlaylist
 }
--- playlist_server.js
+++ playlist_server.js
@@ -30,14 +30,32 @@ app.get("/playlist/:URLname", async (req, res) => {
 
 app.put("/playlist/:URLname/add", async (req, res) => {
     const name = req.params.URLname;
+    const putData = req.body;
     try{
-        await db.insertSong(name);
+        await db.insertSong(name, putData);
         res.status(201).send();
     
     } catch (error) {
         res.status(409).send(error.message);
         console.log(error.message);
     }
+});
+
+app.get("/playlist", async (req, res) => {
+    try{
+        const result = await db.getAllPlaylists("/playlist");
+        res.status(200).json(result);
+    
+    } catch (error) {
+        res.status(404).send(error.message);
+        console.log(error.message);
+    }
+});
+
+app.delete("playlist/:URLname", async (req, res) => {
+    const name = req.params.URLname;
+    console.log(name);
+        await db.deletePlaylist(name);        
 });
 
 app.listen(1337, () => console.log("Playlist Server is running on port 1337"));
